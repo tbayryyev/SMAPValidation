dfd8dbd98da74dee2871fe840ca40a0eeb96b968
Tahyr Bayryyev <100490172+tbayryyev@users.noreply.github.com>
2022-07-01 00:34:22 -0400

    Update csv_to_tiff_singlerasters.py

--- data_processing/csv_to_tiff_singlerasters.py
+++ data_processing/csv_to_tiff_singlerasters.py
@@ -14,14 +14,14 @@ csv_dates = pd.read_csv('Dates.csv') # all the dates of which Soil Moisture data
 #Loop through for all 720 days of data (overwriting the same csv and vrt files to save storage)
 for i in range(1,721):
     
-    csv_input['moisture'] = csv_input2['Day '+str(i)]      # create a new coloumn called 'moisture' and copy the data from MB.csv's coloumn 'Day + i' into it
-    csv_input.to_csv('output.csv', index=False)           # create a new csv file called 'output.csv' with the new coloumn created above
+    csv_input['moisture'] = csv_input2['Day '+str(i)]      # create a new column called 'moisture' and copy the data from MB.csv's column 'Day + i' into it
+    csv_input.to_csv('output.csv', index=False)           # create a new csv file called 'output.csv' with the new column created above
 
-    csv_new = pd.read_csv('output.csv') # read the new csv file
+    csv_new = pd.read_csv('output.csv') # read the new csv file created above
     max_index = 19  # the number of stations in the Millbrook site (max number of Soil Moisture data readings in a day since some sations did not collect data for certain days)
 
 
-    # loop untill all the rows/stations with 0 values are removed from the csv
+    # loop until all the rows/stations with 0 values are removed from the csv
     while(0 in csv_new['moisture'].values):
             j = 0 #index for dropping since it does not update each time when deleting
             m = 0 #index for searching since it updates everytime we delete a row
@@ -30,10 +30,10 @@ for i in range(1,721):
             while(j <= max_index):
                 if (csv_new.iloc[m]['moisture'] == 0):   # check if there is a 0 value meaning there was no data collected at that station
                     csv_new.drop([j],axis = 0,inplace=True) # delete the row if there is a 0 value
-                    j += 1 # increase the row index by 1 since we have gone through one of the rows
+                    j += 1 # increase the row index by 1 since we have looped through the row
                     max_index -= 1 #decrease max station index by 1 since we deleted 1 station
                 else:
-                    j += 1 # increase the row index by 1 since we have gone through one of the rows
+                    j += 1 # increase the row index by 1 since we have looped through the row
                     m +=1 #here we have to increase the index since we want to go to the next row and we did not drop the current row
     
             csv_new.to_csv('output.csv',index=False)  #update the output.csv file with the deleted rows
@@ -52,12 +52,13 @@ for i in range(1,721):
 
 
     
-    #creating the month day and year format for the tiff file name
+    #creating the month day and year format for the tiff file name (need this format to later upload these tiff files into google earth engine)
     day = str(int(csv_dates.iloc[i-1][1]))
     month = str(int(csv_dates.iloc[i-1][2]))
     year = str(int(csv_dates.iloc[i-1][3]))
-
-    if len(day)!= 2: #adds a 0 in front if there is only 1 digit since we want 01 in the date instead of just 1
+    
+    #adds a 0 in front if there is only 1 digit since we want 01 in the date instead of just 1
+    if len(day)!= 2: 
         day = '0'+day
     if len(month)!= 2: 
         month = '0' + month
